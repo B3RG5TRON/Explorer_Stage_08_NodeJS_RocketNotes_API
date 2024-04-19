b1383220a7435013d42c2838574c18fb4929ad92
Eduardo Bergstron <eduardo.goudinho@gmail.com>
2024-04-19 19:47:19 -0300

    :sparkles: Add AppError.js :sparkles:

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,5 +1,23 @@
-class UsersController {
+const AppError = require("../errors/AppError")
 
+
+class UsersController {
+  /*
+    * index - GET para listar vários registros;
+    * show - GET para exibir um registro específico;
+    * create - POST para criar um registro;
+    * update - PUT para atualizar um registro;
+    * delete - DELETE para excluir um registro.
+  */
+  create(request, response) {
+    const { name, email, password } = request.body
+
+    if (!name) {
+      throw new AppError("Nome é obrigatório")
+    }
+
+    response.status(201).json({ name, email, password })
+  }
 }
 
 module.exports = UsersController;
