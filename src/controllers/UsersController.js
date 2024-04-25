211fbbf5bef6bacd2d578b7f735f6c606989d742
Eduardo Bergstron <eduardo.goudinho@gmail.com>
2024-04-24 21:11:16 -0300

    :sparkles: Added create user function

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,22 +1,25 @@
 const AppError = require("../utils/AppError")
 
+const sqliteConnection = require("../database/sqlite-connection")
+
 
 class UsersController {
-  /*
-    * index - GET para listar vários registros;
-    * show - GET para exibir um registro específico;
-    * create - POST para criar um registro;
-    * update - PUT para atualizar um registro;
-    * delete - DELETE para excluir um registro.
-  */
-  create(request, response) {
+  async create(request, response) {
     const { name, email, password } = request.body
 
-    if (!name) {
-      throw new AppError("Nome é obrigatório")
+    const database = await sqliteConnection()
+    const checkUserExists = await database.get("SELECT * FROM users WHERE email = (?)", [email])
+
+    if (checkUserExists) {
+      throw new AppError("Este e-mail já está em uso.")
     }
 
-    response.status(201).json({ name, email, password })
+    await database.run(
+      "INSERT INTO users (name, email, password) VALUES (?, ?, ?)",
+      [name, email, password]
+    )
+
+    return response.status(201).json()
   }
 }
 
